aa8e52f2aae14dbfb5ff181e7b24df61d8350ea6
ericmaeda <ericmaeda41@gmail.com>
2025-11-12 21:42:23 +0700

    fix: Fix news list bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:football_news_mobile/screens/login.dart';
-import 'package:football_news_mobile/screens/menu.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
